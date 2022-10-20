e744c694f3ec134741ae67e04190d6b2bf6bd9f0
Jen <73888356+berryjen@users.noreply.github.com>
2022-10-20 12:50:42 +0200

    fixed syntax issues

--- seeds/create-test-visits.js
+++ seeds/create-test-visits.js
@@ -5,7 +5,11 @@
 exports.seed = async function seed(knex) {
   await knex('visits').del();
   await knex('visits').insert([
-    { user_id: 1, country_id: 1, arrival_time: '04-26-1998', departure_time: '02-18-2019' },
-    { user_id: 2, country_id: 1, arrival_time: '01-01-2008', departure_time: '02-01-2008' },
+    {
+      user_id: 1, country_id: 1, arrival_time: '04-26-1998', departure_time: '02-18-2019',
+    },
+    {
+      user_id: 2, country_id: 1, arrival_time: '01-01-2008', departure_time: '02-01-2008',
+    },
   ]);
 };
