522ffee6b4c83e8dd2d009afcb94e23c0095af57
Jen <73888356+berryjen@users.noreply.github.com>
2022-10-27 16:59:21 +0200

    modified visits timestamp
    differentiated between Date() & new Date()

--- models/visits.js
+++ models/visits.js
@@ -9,7 +9,20 @@ exports.get_all = async () => {
 // get_by_id returns all info regarding a single visit
 exports.get_by_id = async (id) => {
   const visit = await db('visits').where({ id }).first(['id', 'user_id', 'country_id', 'arrival_time', 'departure_time']);
-  visit.arrival_time = visit.arrival_time.toISOString();
-  visit.departure_time = visit.departure_time.toISOString();
+
+  // Parse dates in the DB from strings to number (seconds since UNIX epoch)
+  const atTs = Date.parse(visit.arrival_time);
+  const dtTs = Date.parse(visit.departure_time);
+
+  // Conver these numbers to dates
+  const at = new Date(atTs);
+  const dt = new Date(dtTs);
+
+  // we can now log these
+  console.log('arrival_time', typeof at, at.toISOString());
+  console.log('departure_time', typeof dt, dt.toISOString());
+
+  visit.arrival_time = at.toISOString();
+  visit.departure_time = dt.toISOString();
   return visit;
 };
