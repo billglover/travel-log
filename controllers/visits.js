402d9a555dd435872e45bb14a86b20f23ad0dd33
Jen <73888356+berryjen@users.noreply.github.com>
2022-11-01 16:40:25 +0100

    created POST for visits

--- controllers/visits.js
+++ controllers/visits.js
@@ -9,3 +9,23 @@ exports.get = async (req, res) => {
   const visit = await visitsModel.get_by_id(req.params.id);
   return res.json(visit);
 };
+
+exports.create = async (req, res) => {
+  try {
+    const visit = await visitsModel.create(
+      req.body.user_id,
+      req.body.country_id,
+      req.body.arrival_time,
+      req.body.departure_time,
+    );
+    return res.status(201).json(visit);
+  } catch (err) {
+    if (err instanceof visitsModel.ConstraintIdNullError) {
+      err.status = 400;
+    } else {
+      console.log('unable to create visit:', err);
+      err.message = 'unable to create visit';
+    }
+    throw err;
+  }
+};
